f2a9a98ec06bfa0e381bd34a22f8f05974b54516
Arnor Ameti <arnameti@gmail.com>
2022-11-21 12:47:12 +0100

    feat: adding click event to the buttons

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
-if (module.hot) {
-  module.hot.accept();
-}
+'use strict';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+const calculatorEl = document.querySelector('[data-calculator]');
+console.log(calculatorEl);
+
+calculatorEl.addEventListener('click', function (e) {
+  const clicked = e.target.closest('[data-button]');
+
+  if (!clicked) return;
+
+  console.log(clicked);
+});
