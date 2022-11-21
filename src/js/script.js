04693550c4a46783af6e9655013c11e5340bc2e0
Arnor Ameti <arnameti@gmail.com>
2022-11-21 18:44:39 +0100

    feat: adding highlighting functionality to the operator btns

--- src/js/script.js
+++ src/js/script.js
@@ -7,21 +7,20 @@
 const calculatorEl = document.querySelector('[data-calculator]');
 const displayedResultEl = calculatorEl.querySelector('[data-result]');
 
+const clickedValue = clicked.textContent;
+const displayedValue = +displayedResultEl.textContent;
+const buttonType = clicked.dataset.button;
+const previousKeyType = calculatorEl.dataset.previousKeyType;
+
+const displayResult = value => (displayedResultEl.textContent = value);
+const savePrevClickedBtn = buttonType =>
+  (calculatorEl.dataset.previousKeyType = buttonType);
+
 calculatorEl.addEventListener('click', function (e) {
   const clicked = e.target.closest('[data-button]');
 
   if (!clicked) return;
 
-  const clickedValue = clicked.textContent;
-  const displayedValue = +displayedResultEl.textContent;
-  const buttonType = clicked.dataset.button;
-  const previousKeyType = calculatorEl.dataset.previousKeyType;
-
-  const displayResult = value => (displayedResultEl.textContent = value);
-  const savePrevClickedBtn = buttonType =>
-    (calculatorEl.dataset.previousKeyType = buttonType);
-
-  // if clicked button is a number
   if (buttonType === 'number') {
     if (displayedValue === 0 || previousKeyType === 'operator') {
       displayResult(clickedValue);
@@ -30,8 +29,17 @@ calculatorEl.addEventListener('click', function (e) {
     }
   }
 
-  // if clicked button is an operator
+  // prettier-ignore
   if (buttonType === 'operator') {
+    //select highlighted element and remove the class to remove the highlighting
+    const currentSelectedOperator = calculatorEl.querySelector('.calculator__operator--selected');
+    currentSelectedOperator?.classList.remove('calculator__operator--selected');
+
+    // add class to operator-btn, to add the highlight
+    clicked.classList.add('calculator__operator--selected');
+  }
+
+  if (buttonType === 'equal') {
   }
 
   savePrevClickedBtn(buttonType);
