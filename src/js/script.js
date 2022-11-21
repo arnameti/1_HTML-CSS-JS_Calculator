5d237553651f7d4951beb6eda608ec3b2e64a5a0
Arnor Ameti <arnameti@gmail.com>
2022-11-21 16:27:25 +0100

    feat: adding functionality to show only numbers in the output

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,21 @@
 // }
 
 const calculatorEl = document.querySelector('[data-calculator]');
+const displayedResultEl = calculatorEl.querySelector('[data-result]');
 
 calculatorEl.addEventListener('click', function (e) {
   const clicked = e.target.closest('[data-button]');
 
   if (!clicked) return;
 
-  console.log(clicked);
+  const clickedValue = clicked.textContent;
+  const displayedValue = +displayedResultEl.textContent;
+
+  if (clicked.dataset.button === 'number') {
+    if (displayedValue === 0) {
+      displayedResultEl.textContent = clickedValue;
+    } else {
+      displayedResultEl.textContent = displayedValue + clickedValue;
+    }
+  }
 });
